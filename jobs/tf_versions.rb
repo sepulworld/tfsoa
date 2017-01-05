3e8acdf491a90a1d363cdc1106e3249e15de0f27
zane <zane.williamson@gmail.com>
2017-01-04 22:47:25 -0800

    vetted query to grab state_details for tf version

--- jobs/tf_versions.rb
+++ jobs/tf_versions.rb
@@ -1,7 +1,10 @@
 require_relative '../lib/tfsoa'
 
+# This should pick up the latest state_details for a given tfstates entry
+
+# tf = Tfstate.all
+# tf.each do |a| puts a.state_details.first.terraform_version end
+
 # Grab the unique number of s3 bucket terraform states registered in DB
 #SCHEDULER.every '10s' do
-  #tf_state_count = Tfstate.select(:s3_bucket_key).distinct.count
-  #send_event('number_of_states', { current: tf_state_count })
 #end
