68cb4798565b17843edba4a646ee12cd5498980e
zane <zane.williamson@gmail.com>
2017-01-07 23:24:41 -0800

    Adding tf_versions chart

--- jobs/tf_versions.rb
+++ jobs/tf_versions.rb
@@ -1,13 +1,19 @@
 require_relative '../lib/tfsoa'
 
-tf_versions = Array.new
-tf_version_counts = Hash.new({ value: 0 })
-
-# This should pick up the latest state_details for a given tfstates entry
-
-# tf = Tfstate.all
-# tf.each do |a| puts a.state_details.first.terraform_version end
-
-# Grab the unique number of s3 bucket terraform states registered in DB
-#SCHEDULER.every '10s' do
-#end
+SCHEDULER.every '10s' do
+  tf = Tfstate.all
+  tf_versions = Array.new
+  tf_version_counts = Hash.new 0 
+  tf_version_labels = Hash.new({ value: 0 })
+  tf.each do |state|
+    tf_versions.push(state.state_details.first.terraform_version)
+  end
+  tf_versions.each do |count|
+    tf_version_counts[count] += 1
+  end
+  tf_version_counts.each do |label, value|
+    version = label
+    tf_version_labels[version] = { label: "#{version}", value: value}
+  end
+  send_event('tf_versions', { items: tf_version_labels.values })
+end
