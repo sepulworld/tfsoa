a38b5e41ea6bc6629eff6b75aa8d5435c13028e4
Zane Williamson <sepulworld@gmail.com>
2017-01-08 18:18:14 -0800

    Adding active_states tracking (#4)
    * Adding active_states tracking
    
    * updating screenshot for readme

--- jobs/active_states.rb
+++ jobs/active_states.rb
@@ -1,3 +1,21 @@
 require_relative '../lib/tfsoa'
+require 'active_support/all'
 
-#Place holder for states with 5 or more updates in last 48 hours
+#TF States with 5 or more updates in last day hours
+SCHEDULER.every '10s' do
+  tf_states_with_changes = Hash.new 0
+  tf_states_with_5_or_more_changes = Hash.new 0
+  states = StateDetail.where(created_at: (Time.now.prev_day)..(Time.now))
+  states.each do |state|
+    tf_states_with_changes[Tfstate.where(
+      id: state.tfstate_id).take.s3_bucket_key] += 1
+  end
+  tf_states_with_changes.each do |state, updates|
+    if updates >= 5
+      tf_states_with_5_or_more_changes[state] = { label: "#{state}",
+                                                  value: updates }
+    end
+  end
+  send_event('active_states',
+    { items: tf_states_with_5_or_more_changes.values })
+end
