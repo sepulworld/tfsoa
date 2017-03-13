c678777703cd6b833bafeaba3494248f664cc509
Zane Williamson <sepulworld@gmail.com>
2017-03-13 13:44:22 -0700

    [WIP] Updating add_tf_state post api (#8)
    * Updating add_tf_state post api
    
    * Cleanup
    
    * moving test state into fixtures

--- lib/tfsoa.rb
+++ lib/tfsoa.rb
@@ -24,21 +24,6 @@ class TerraformSOA < Sinatra::Base
   register Sinatra::ActiveRecordExtension
   set :database_file, "../config/database.yml"
 
-  if File.exist?("/etc/tfsoa.yaml")
-    config = YAML.load_file("/etc/tfsoa.yaml")
-    @aws_access_key_id = config["aws_creds"]["aws_access_key_id"]
-    @aws_secret_access_key = config["aws_creds"]["aws_secret_access_key"]
-  end
-
-  def assume_role(role_arn)
-    role_credentials = Aws::AssumeRoleCredentials.new(
-      client: Aws::STS::Client.new,
-      role_arn: role_arn,
-      role_session_name: "tfsoa-read"
-    )
-    return role_credentials
-  end
-
   def extract_tf_version(state)
      state['terraform_version']
   end
@@ -51,16 +36,8 @@ class TerraformSOA < Sinatra::Base
      state['serial']
   end
 
-  def find_s3_bucket_key_entry(s3_bucket_key)
-    Tfstate.find_by s3_bucket_key: s3_bucket_key
-  end
-
-  def load_tf_state_raw_json(s3_bucket_name, s3_bucket_key, role_arn)
-    role_credentials = assume_role(role_arn)
-    s3 = Aws::S3::Client.new(credentials: role_credentials)
-    resp = s3.get_object(bucket: s3_bucket_name, key: s3_bucket_key)
-    raw_state = resp.body.read
-    return raw_state
+  def find_unique_tf_state_entry(unique_tf_state)
+    Tfstate.find_by unique_tf_state: unique_tf_state
   end
 
   def load_tf_state_ruby_hash(raw_state)
@@ -68,8 +45,7 @@ class TerraformSOA < Sinatra::Base
     return tf_state
   end
 
-  def create_state_detail_entry(db_transaction,
-                                raw_state, state, s3_bucket_name, s3_bucket_key)
+  def create_state_detail_entry(db_transaction, raw_state, state)
     db_transaction.state_details.create(
       state_json: raw_state,
       terraform_version: extract_tf_version(state),
@@ -78,16 +54,22 @@ class TerraformSOA < Sinatra::Base
       )
   end
 
-  def create_tf_entry(state, raw_state, s3_bucket_name, s3_bucket_key, role_arn)
-    db_transaction = find_s3_bucket_key_entry(s3_bucket_key)
+  def create_tf_entry(state, raw_state, team, product, service, environment)
+    unique_tf_state = "#{team}-#{product}-#{service}-#{environment}"
+    db_transaction = find_s3_bucket_key_entry(unique_tf_state)
     if db_transaction.nil?
-      db_transaction = Tfstate.create(
-        s3_bucket_uri: "s3://#{s3_bucket_name}/#{s3_bucket_key}",
-        s3_bucket_key: s3_bucket_key,
-        role_arn: role_arn)
+      db_transaction = Tfstate.create(unique_tf_state: unique_tf_state)
     end
-    create_state_detail_entry(db_transaction,
-                              raw_state, state, s3_bucket_name, s3_bucket_key)
+    create_state_detail_entry(db_transaction, raw_state, state)
+  end
+
+  def valid_json?(json)
+    begin
+      JSON.parse(json)
+      return true
+    rescue JSON::ParserError
+      return false
+    end
   end
 
   set(:method) do |method|
@@ -101,26 +83,21 @@ class TerraformSOA < Sinatra::Base
 
   get '/outputs/*' do
     # Example
-    # http://127.0.0.1:9292/tfsoa/outputs/autozane_kafka_awslogs_cloudwatch%2Fpromotion%2FTerraform
-    @state_entry = Tfstate.find_by s3_bucket_key: params[:splat]
+    # http://127.0.0.1:9292/tfsoa/outputs/dataplatform/silverbullet/zookeeper/dev
+    @state_entry = Tfstate.find_by unique_tf_state: params[:splat]
     @state_entry_last_details = @state_entry.state_details.last
     @state_entry_json = JSON.parse(@state_entry_last_details.state_json)
     erb :outputs
   end
 
-  post '/add_tf_state' do
-    # example JSON input body
-    # {
-    #   "role_arn": "arn:aws:iam::357170183134:role/s3read",
-    #   "s3_bucket_name": "terraform-autozane-remote-state",
-    #   "s3_bucket_key": "autozane_kafka_awslogs_cloudwatch/promotion/Terraform"
-    # }
-    s3_bucket_name = @req_data['s3_bucket_name']
-    s3_bucket_key = @req_data['s3_bucket_key']
-    role_arn = @req_data['role_arn']
-    raw_state = load_tf_state_raw_json(s3_bucket_name, s3_bucket_key, role_arn)
-    state = load_tf_state_ruby_hash(raw_state)
-    create_tf_entry(state, raw_state, s3_bucket_name, s3_bucket_key, role_arn)
+  post '/add_tf_state/:team/:product/:service/:environment/' do
+    # HTTP post TF state json to then endpoint
+    @params = params
+    valid_json(@req_data)
+    state = load_tf_state_ruby_hash(@req_data)
+    raw_state = @req_data
+    create_tf_entry(state, raw_state, @params[:team], @params[:product],
+      @params[:service], @params[:environment])
   end
 
 end
