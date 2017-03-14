9e57283bbc11ea24a569a2f2554958bbeb1a2f61
Ian Morgan <ianm280+1@gmail.com>
2017-03-14 16:32:58 -0700

    change from active states to recent

--- jobs/active_states.rb
+++ jobs/active_states.rb
@@ -3,19 +3,7 @@ require 'active_support/all'
 
 #TF States with 5 or more updates in last day hours
 SCHEDULER.every '10s' do
-  tf_states_with_changes = Hash.new 0
-  tf_states_with_5_or_more_changes = Hash.new 0
-  states = StateDetail.where(created_at: (Time.now.prev_day)..(Time.now))
-  states.each do |state|
-    tf_states_with_changes[Tfstate.where(
-      id: state.tfstate_id).take.unique_tf_state] += 1
-  end
-  tf_states_with_changes.each do |state, updates|
-    if updates >= 5
-      tf_states_with_5_or_more_changes[state] = { label: "#{state}",
-                                                  value: updates }
-    end
-  end
+  tf_states_with_changes_labelled = StateDetail.last(5).map {|a| {label: a.tfstate.unique_tf_state, value: a.created_at.strftime("%FT%R")}}
   send_event('active_states',
-    { items: tf_states_with_5_or_more_changes.values })
+    { items: tf_states_with_changes_labelled })
 end
