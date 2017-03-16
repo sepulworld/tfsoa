4a610e4e13208d53b7fd14f7c62aa6f738ee3b8c
Zane Williamson <sepulworld@gmail.com>
2017-03-16 15:52:09 -0700

    Zmw/adding team column (#18)
    * Adding team column
    
    * Adding erb for team states
    
    * adding link back to team states
    
    * match up with Ian's pr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170316190000) do
+ActiveRecord::Schema.define(version: 20170316214522) do
 
   create_table "state_details", force: :cascade do |t|
     t.integer  "tfstate_id"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170316190000) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "unique_tf_state"
+    t.string   "team"
   end
 
 end
