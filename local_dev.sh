3ed32863a08498e1b334f45942160916f3e404ed
zane <zane.williamson@gmail.com>
2017-05-19 16:49:01 -0700

    Updating local_dev.sh functions

--- local_dev.sh
+++ local_dev.sh
@@ -1,27 +1,36 @@
 #!/bin/bash
 
-if [ -f /usr/bin/terraform ]
-  then
-    echo "Terraform installed"
-  else
-    wget https://releases.hashicorp.com/terraform/0.9.5/terraform_0.9.5_linux_amd64.zip
-    unzip terraform_0.9.5_linux_amd64.zip -d /usr/bin/
-fi
+function install_system_packages {
+  apt-get update
+  apt-get install -y graphviz \
+    ruby \
+    ruby-dev \
+    libsqlite3-dev \
+    build-essential \
+    libmysqlclient-dev \
+    nodejs
+}
 
-apt-get update
-apt-get install -y graphviz \
-  ruby \
-  ruby-dev \
-  libsqlite3-dev \
-  build-essential \
-  libmysqlclient-dev \
-  nodejs
+function install_terraform {
+  if [ -f /usr/bin/terraform ]
+    then
+      echo "Terraform installed"
+    else
+      wget --quiet https://releases.hashicorp.com/terraform/0.9.5/terraform_0.9.5_linux_amd64.zip
+      unzip terraform_0.9.5_linux_amd64.zip -d /usr/bin/
+  fi
+}
 
-gem install bundler puma
-cd /vagrant
-bundle install
-bundle exec rake db:setup
-cat << EOF > /etc/systemd/system/tfsoa.service
+function install_gems {
+  gem install bundler puma
+  pushd /vagrant || exit
+  bundle install
+  bundle exec rake db:setup
+  popd
+}
+
+function setup_systemd {
+  cat << EOF > /etc/systemd/system/tfsoa.service
 [Unit]
 Description=TFSOA server
 After=network.target
@@ -36,35 +45,47 @@ Type=simple
 WantedBy=multi-user.target
 Alias=tfsoa.service
 EOF
+  /bin/systemctl daemon-reload
+  /bin/systemctl restart tfsoa
+  sleep 6
+}
 
-/bin/systemctl daemon-reload
-/bin/systemctl restart tfsoa
+function setup_test_states_and_graphs {
+  echo "Submit test Terraform states..."
+  curl 127.0.0.1:9292/tfsoa/add_tf_state/example_team_1/product1/example1/dev/ \
+    --silent \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d @/vagrant/test/fixtures/state1
 
-sleep 6
-echo "Submit test Terraform states..."
-curl 127.0.0.1:9292/tfsoa/add_tf_state/example_team_1/product1/example1/dev/ \
-  --silent \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d @test/fixtures/state1
+  curl 127.0.0.1:9292/tfsoa/add_tf_state/example_team_2/product2/example1/dev/ \
+    --silent \
+    -H "Content-Type: application/json" \
+    -X POST \
+    -d @/vagrant/test/fixtures/state1
 
-curl 127.0.0.1:9292/tfsoa/add_tf_state/example_team_2/product2/example1/dev/ \
-  --silent \
-  -H "Content-Type: application/json" \
-  -X POST \
-  -d @test/fixtures/state1
+  pushd /vagrant/test/fixtures/tf_elb
+  /usr/bin/terraform graph -draw-cycles \
+    | curl -d @- \
+    http://127.0.0.1:9292/tfsoa/add_tf_graph/example_team_1/product1/example1/dev/ \
+    --silent
+  popd
 
-pushd test/fixtures/tf_elb
-terraform graph -draw-cycles \
-  | curl -d @- \
-  http://127.0.0.1:9292/tfsoa/add_tf_graph/example_team_1/product1/example1/dev/ \
-  --silent
-popd
+  pushd /vagrant/test/fixtures/tf_asg
+  /usr/bin/terraform graph -draw-cycles \
+    | curl -d @- \
+    http://127.0.0.1:9292/tfsoa/add_tf_graph/example_team_2/product2/example1/dev/ \
+    --silent
+  echo "Navigate to http://127.0.0.1:9292"
+}
 
-pushd test/fixtures/tf_asg
-terraform graph -draw-cycles \
-  | curl -d @- \
-  http://127.0.0.1:9292/tfsoa/add_tf_graph/example_team_2/product2/example1/dev/ \
-  --silent
+function main {
+  install_system_packages
+  install_terraform
+  install_gems
+  setup_systemd
+  setup_test_states_and_graphs
+}
 
-echo "Navigate to http://127.0.0.1:9292"
+# entry point
+main
