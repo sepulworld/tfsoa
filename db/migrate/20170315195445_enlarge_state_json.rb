7f61351c543118f87f163559ee3d663620e1d5da
zane <zane.williamson@gmail.com>
2017-05-23 23:26:07 -0700

    reduce table size

--- db/migrate/20170315195445_enlarge_state_json.rb
+++ db/migrate/20170315195445_enlarge_state_json.rb
@@ -1,5 +1,5 @@
 class EnlargeStateJson < ActiveRecord::Migration[5.0]
   def change
-    change_column :state_details, :state_json, :text, :limit => 4294967295
+    change_column :state_details, :state_json, :text, :limit => 1073741824
   end
 end
