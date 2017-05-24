edb7ad0efd40cd8992583d52a9d7c0e376896e49
zane <zane.williamson@gmail.com>
2017-05-23 23:30:43 -0700

    reduce size

--- db/migrate/20170315195445_enlarge_state_json.rb
+++ db/migrate/20170315195445_enlarge_state_json.rb
@@ -1,5 +1,5 @@
 class EnlargeStateJson < ActiveRecord::Migration[5.0]
   def change
-    change_column :state_details, :state_json, :text, :limit => 1073741824
+    change_column :state_details, :state_json, :text, :limit => 966367641
   end
 end
--- db/migrate/20170317180035_enlarge_digraph.rb
+++ db/migrate/20170317180035_enlarge_digraph.rb
@@ -1,5 +1,5 @@
 class EnlargeDigraph < ActiveRecord::Migration[5.0]
   def change
-    change_column :state_details, :digraph, :text, :limit => 1073741824
+    change_column :state_details, :digraph, :text, :limit => 966367641
   end
 end
